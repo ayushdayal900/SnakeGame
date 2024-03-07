4c5ca85f7939c16d84bb7a3b57abdb8c8c621023
ayushdayal900 <134316930+ayushdayal900@users.noreply.github.com>
2024-03-07 15:10:14 +0530

    Update SnakeGame.java

--- SnakeGame/SnakeGame.java
+++ SnakeGame/SnakeGame.java
@@ -2,21 +2,22 @@
 package SnakeGame;
 import javax.swing.*;
 
+//JFrame class consist function regarding creation of frame so inheriting it in main class
 public class SnakeGame extends JFrame{
     
        SnakeGame(){
-           super("Snake Game");
-           add(new Board());//..........add method add board on frame
-           pack();//....................refrashing the frame many times
+           super("Snake Game");//..initializing the heading of frame
+           add(new Board());//...........add method add board on frame
+           pack();//.....................refrashing the frame many times
            
-           setVisible(true);//..........refrashing the frame one times
-           setSize(1000,1000);
-           setLocationRelativeTo(null);
+           setVisible(true);//.................refrashing the frame one times
+           setSize(1000,1000);//....initializing the size of frame
+           setLocationRelativeTo(null);//......locating the frame to center
            
        }
     
     public static void main(String[] args) {
-           new SnakeGame();
+           new SnakeGame(); //creation of object of SnakeGame so that constructor gets called
     }
     
 }
